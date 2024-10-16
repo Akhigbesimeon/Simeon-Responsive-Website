a2e6a73d8868a5f3cc7477102305ef46abf7277b
Sakhigbe <90878748+Akhigbesimeon@users.noreply.github.com>
2024-10-16 20:47:28 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+// Menu toggle
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');  // Toggle 'bx-x' class to change the menu icon
+    navbar.classList.toggle('active');  // Toggle active class to show/hide the navbar
+}
+
+// Scroll section active link
+window.onscroll = () => {
+    let top = window.scrollY;
+    sections.forEach(sec => {
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
+};
